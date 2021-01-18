34b8de23258c9aa86ee72cfcccac8f7e1bbd10b1
ccj1994 <a0changj2@gmail.com>
2021-01-18 16:20:29 +0800

    刻版面

---
+++ backend/add_admin.php
@@ -0,0 +1,25 @@
+<h2 class="ct">新增管理帳號</h2>
+<table>
+  <tr>
+    <td>帳號</td>
+    <td><input type="text" name="acc"></td>
+  </tr>
+  <tr>
+    <td>密碼</td>
+    <td><input type="password" name="pw"></td>
+  </tr>
+  <tr>
+    <td>權限</td>
+    <td>
+      <input type="checkbox" name="">
+      <input type="checkbox" name="">
+      <input type="checkbox" name="">
+      <input type="checkbox" name="">
+      <input type="checkbox" name="">
+    </td>
+  </tr>
+</table>
+<div class="ct">
+  <button type="submit">新增</button>
+  <button type="reset">重置</button>
+</div>
