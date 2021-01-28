5860865a2960ea01a0732dcf7ae85ce0ec918c66
ccj1994 <a0changj2@gmail.com>
2021-01-28 13:11:19 +0800

    完成新增修改商品及前端商品選單

---
+++ api/add_goods.php
@@ -0,0 +1,11 @@
+<?php
+include_once "../base.php";
+if(!empty($_FILES['img']['tmp_name'])){
+  $_POST['img']=$_FILES['img']['name'];
+}
+
+$_POST['num']=rand(10000,999999);
+$_POST['sh']=1;
+$Goods->save($_POST);
+to("../backend.php?do=th");
+?>
